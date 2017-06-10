76ebacbd098722ef20f18fa895e12c76d0b73c81
Mitcheljager <mitchel_jager@hotmail.com>
2017-06-10 19:31:23 +0200

    Added signup and login

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,11 @@ Rails.application.routes.draw do
 
   resources :posts
   get 'search', to: 'posts#search'
+
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
 end

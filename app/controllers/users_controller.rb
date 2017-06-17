bf35494c84cc502d90837bf0ee79d88d78af7f83
Mitcheljager <mitchel_jager@hotmail.com>
2017-06-17 04:01:27 +0200

    Added user specific posts to profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,6 +14,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.select("id", "name", "created_at").find_by_name(params[:name])
+    @posts = Post.where(user_id: @user.id).all
   end
 
   private

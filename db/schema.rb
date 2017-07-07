d73cf7e2b557876dda788c82175fef8bd913cc83
Mitcheljager <mitchel_jager@hotmail.com>
2017-07-07 22:38:25 +0200

    Add migrations for extra fields in posts and user

--- db/schema.rb
+++ db/schema.rb
@@ -10,23 +10,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170617014433) do
+ActiveRecord::Schema.define(version: 20170707203329) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
     t.string   "content"
     t.string   "categories"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
     t.integer  "user_id"
+    t.string   "price"
+    t.string   "description"
+    t.string   "image"
   end
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
     t.string   "password_digest"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at",                  null: false
+    t.datetime "updated_at",                  null: false
+    t.integer  "user_level",      default: 0, null: false
   end
 
 end

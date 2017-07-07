cbb5e127e7115ba1eefd8ac443e0b82b708eb0ce
Mitcheljager <mitchel_jager@hotmail.com>
2017-07-07 22:47:02 +0200

    Added real data to posts overview and post show

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170707203329) do
+ActiveRecord::Schema.define(version: 20170707204259) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20170707203329) do
     t.string   "price"
     t.string   "description"
     t.string   "image"
+    t.string   "license"
   end
 
   create_table "users", force: :cascade do |t|
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,5 +8,5 @@
 
 User.create!(name: "admin", email: "admin@example.com", password_digest: "admin", user_level: 1)
 
-Post.create!(title: "Sample Post", content: "Sample Content", categories: "category", price: "14", description: "This is a short description", image: "http://placehold.it/600x400")
-Post.create!(title: "Another Post", content: "Post 2", categories: "category", price: "20", description: "This is a short description", image: "http://placehold.it/600x400")
+Post.create!(title: "Sample Post", content: "Sample Content", categories: "category", price: "14", description: "This is a short description", image: "http://placehold.it/600x400", license: "Standard license")
+Post.create!(title: "Another Post", content: "Post 2", categories: "category", price: "20", description: "This is a short description", image: "http://placehold.it/600x400", license: "Standard license")

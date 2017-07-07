66d9848a4a317fa75beafdf3430d84c87b15eed9
Mitcheljager <mitchel_jager@hotmail.com>
2017-07-07 22:51:50 +0200

    Added fields to /posts/new

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -29,6 +29,6 @@ class PostsController < ApplicationController
 
   private
     def post_params
-      params.require(:post).permit :title, :content, :categories
+      params.require(:post).permit :title, :content, :categories, :price, :image, :license
     end
 end

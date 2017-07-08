5907da38e46893c3eed880486925b45d125cd476
Mitcheljager <mitchel_jager@hotmail.com>
2017-07-08 14:49:50 +0200

    Add user_name to posts. Force validation in posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,19 @@
 class PostsController < ApplicationController
+  before_action :authorize, except: [:show, :index, :search]
+
   def index
     @posts = Post.all
+
+    @posts.each do |post|
+      user = User.select("id", "name").find_by_id(post.user_id)
+      post.user_name = user.name
+    end
   end
 
   def show
     @post = Post.find params[:id]
+    user = User.select("id", "name").find_by_id(@post.user_id)
+    @post.user_name = user.name
   end
 
   def new
@@ -12,6 +21,8 @@ class PostsController < ApplicationController
 
   def create
     @post = Post.new post_params
+    @post.user_id = current_user.id
+
     if @post.save
       redirect_to @post
     else
